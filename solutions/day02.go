b7743fc2575a4682f25eb13164afe6dfea1d6890
Balaji Sivaraman <balaji@balajisivaraman.com>
2021-12-07 20:41:43 +0530

    feat: add solution for Day 02, Part 2

--- solutions/day02.go
+++ solutions/day02.go
@@ -10,33 +10,47 @@ type Command struct {
 	Amount int
 }
 
-func (command *Command) isMoveForward() bool {
+func (command *Command) isForwardCommand() bool {
 	return command.Movement == "forward"
 }
 
-func (command *Command) isMoveDown() bool {
+func (command *Command) isDownCommand() bool {
 	return command.Movement == "down"
 }
 
-func (command *Command) isMoveUp() bool {
+func (command *Command) isUpCommand() bool {
 	return command.Movement == "up"
 }
 
 type Coordinate struct {
 	HorizontalPosition int
 	Depth int
+	Aim int
 }
 
-func (coordinate *Coordinate) moveForward(by int) {
+func (coordinate *Coordinate) moveForwardWrong(by int) {
 	coordinate.HorizontalPosition += by
 }
 
-func (coordinate *Coordinate) moveDown(by int) {
+func (coordinate *Coordinate) moveDownWrong(by int) {
 	coordinate.Depth += by
 }
 
-func (coordinate *Coordinate) moveUp(by int) {
+func (coordinate *Coordinate) moveUpWrong(by int) {
 	coordinate.Depth -= by
+}
+
+func (coordinate *Coordinate) increaseAim(by int) {
+	coordinate.Aim += by
+}
+
+func (coordinate *Coordinate) decreaseAim(by int) {
+	coordinate.Aim -= by
+}
+
+func (coordinate *Coordinate) forward(by int) {
+	coordinate.HorizontalPosition += by
+	coordinate.Depth += (coordinate.Aim * by)
 }
 
 func Day02Part1(input []string) int {
@@ -44,6 +58,45 @@ func Day02Part1(input []string) int {
 		HorizontalPosition: 0,
 		Depth: 0,
 	}
+	commands := parseInputToCommand(input)
+	for i := 0; i < len(commands); i++ {
+		command := commands[i]
+		if command.isForwardCommand() {
+			coordinate.moveForwardWrong(command.Amount)
+		}
+		if command.isDownCommand() {
+			coordinate.moveDownWrong(command.Amount)
+		}
+		if command.isUpCommand() {
+			coordinate.moveUpWrong(command.Amount)
+		}
+	}
+	return coordinate.HorizontalPosition * coordinate.Depth
+}
+
+func Day02Part2(input []string) int {
+	coordinate := Coordinate {
+		HorizontalPosition: 0,
+		Depth: 0,
+		Aim: 0,
+	}
+	commands := parseInputToCommand(input)
+	for i := 0; i < len(commands); i++ {
+		command := commands[i]
+		if command.isForwardCommand() {
+			coordinate.forward(command.Amount)
+		}
+		if command.isDownCommand() {
+			coordinate.increaseAim(command.Amount)
+		}
+		if command.isUpCommand() {
+			coordinate.decreaseAim(command.Amount)
+		}
+	}
+	return coordinate.HorizontalPosition * coordinate.Depth
+}
+
+func parseInputToCommand(input []string) []Command {
 	commands := []Command{}
 	for i := 0; i < len(input); i++ {
 		split := strings.Split(input[i], " ")
@@ -53,17 +106,5 @@ func Day02Part1(input []string) int {
 		}
 		commands = append(commands, command)
 	}
-	for i := 0; i < len(commands); i++ {
-		command := commands[i]
-		if command.isMoveForward() {
-			coordinate.moveForward(command.Amount)
-		}
-		if command.isMoveDown() {
-			coordinate.moveDown(command.Amount)
-		}
-		if command.isMoveUp() {
-			coordinate.moveUp(command.Amount)
-		}
-	}
-	return coordinate.HorizontalPosition * coordinate.Depth
+	return commands
 }
--- solutions/day02_test.go
+++ solutions/day02_test.go
@@ -12,3 +12,12 @@ func TestDay02Part1(t *testing.T) {
 		t.Errorf("Day02: Part 1(%s) = %d, want %d", input, actual, want)
 	}
 }
+
+func TestDay02Part2(t *testing.T) {
+	input := []string{"forward 5", "down 5", "forward 8", "up 3", "down 8", "forward 2"}
+	want := 900
+	actual := Day02Part2(input)
+	if !(actual == want) {
+		t.Errorf("Day02: Part 2(%s) = %d, want %d", input, actual, want)
+	}
+}
